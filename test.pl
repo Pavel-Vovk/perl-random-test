5c926d95f50caac3982d8d416a7340553b90bf37
Pavel Vovk <pavel.vovk.work@gmail.com>
2019-01-23 15:29:21 +0200

    Update test.pl

--- test.pl
+++ test.pl
@@ -6,6 +6,6 @@ ok 2, 'Always passing 2';
 #ok 0, "Always failing";
 ok 1, 'Always passing';
 ok 2, 'Always passing 2';
-my $rand = int rand(2);
+my $rand = int rand(2)-1;
 ok $rand, "Random failing $rand";
 ok $rand, "Random failing $rand";
