910b3e03cb6ed049190625845b375eb70ff30dd0
Pavel Vovk <pavel.vovk.work@gmail.com>
2019-01-23 14:34:40 +0200

    Update test.pl

--- test.pl
+++ test.pl
@@ -1,4 +1,5 @@
 use strict;
+use Test::Unit::Runner::XML;
 use Test::More tests => 5;
 
 ok 1, 'Always passing';
@@ -9,3 +10,7 @@ ok 2, 'Always passing 2';
 my $rand = int rand(2);
 ok $rand, 'Random failing';
 ok $rand, 'Random failing';
+mkdir("test_reports");
+my $runner = Test::Unit::Runner::XML->new("test-reports");
+$runner->start($test);
+exit(!$runner->all_tests_passed());
